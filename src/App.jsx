cdaf82c291d8a290f32b772ae45ae0bea4d26e45
Manish <manish2003sharma@gmail.com>
2025-06-25 14:58:25 +0530

    Personal Finance Tracker

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Header from './components/Header/Header'
+import './App.css'
 
 const App = () => {
     return (
         <div>
-            <h1>Personal Finance Tracker</h1>
+            <Header />
         </div>
     )
 }
