ec8b1634417c3379d47c10bbee3c80edc9b93963
Manish <manish2003sharma@gmail.com>
2025-06-25 14:57:02 +0530

    Header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div className='navbar'>
+            <h1>Header</h1>
+            {/* <p>This is header</p> */}
+        </div>
+    )
+}
+
+export default Header
