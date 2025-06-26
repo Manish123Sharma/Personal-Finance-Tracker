8a7c5892a2eda01d6392c8afec701096f23c177f
Manish <manish2003sharma@gmail.com>
2025-06-26 14:10:38 +0530

    Login Register

---
+++ src/components/Login_Register/LoginRegister.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './LoginRegister.css'
+
+const LoginRegister = () => {
+    return (
+        <div className='register_wrapper'>
+            <h2 className='title'>
+                Register on{' '}
+                <span style={{ color: "var(--theme)" }}>
+                    Financy
+                </span>
+            </h2>
+        </div>
+    );
+}
+
+export default LoginRegister
