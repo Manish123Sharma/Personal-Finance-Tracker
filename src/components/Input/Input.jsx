81d8b0cc3f93518ac5e78bf065dc38953561fcfb
Manish <manish2003sharma@gmail.com>
2025-06-26 14:11:19 +0530

    Input

---
+++ src/components/Input/Input.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Input.css'
+
+const Input = () => {
+    return (
+        <div className='input_wrapper'>
+            <input type="text" className='input_field' placeholder='Enter text...' />
+        </div>
+    );
+}
+
+export default Input
