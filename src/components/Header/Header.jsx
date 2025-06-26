7763a0380446e3fad9f91e5414f4ba2940e49f29
Manish <manish2003sharma@gmail.com>
2025-06-26 14:10:05 +0530

    Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
 import './Header.css'
+import logo from '../../assets/logo.svg'
 
 const Header = () => {
+    const handlelogout = () => {
+        console.log(`Logging out...`);
+        alert('Logout successful');
+    }
     return (
         <div className='navbar'>
-            <h1>Header</h1>
+            <div className='logo'>
+                <img src={logo} alt="Logo" />
+                <div className='logo-text'>
+                    <h1>Financy</h1>
+                    <span>Personal Finance Tracker</span>
+                </div>
+            </div>
+            <button
+                type="button"
+                className="logout-button"
+                onClick={() => handlelogout()}
+            >
+                ⏻ Logout
+            </button>
             {/* <p>This is header</p> */}
         </div>
     )
