9588d7c0fafc883c8f6e98310c624da62d63e4cf
Manish <manish2003sharma@gmail.com>
2025-06-26 14:11:47 +0530

    Personal Finance Tracker

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import React from 'react'
-import Header from './components/Header/Header'
+// import Header from './components/Header/Header'
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import './App.css'
+// import './index.css'
+import SignUp from './pages/SignUp'
+import Dashboard from './pages/Dashboard'
 
 const App = () => {
     return (
-        <div>
-            <Header />
-        </div>
-    )
+        <Router>
+            <Routes>
+                <Route path='/' element={<SignUp />} />
+                <Route path='/dashboard' element= {<Dashboard />} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App
