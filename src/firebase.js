3d405b876cefc42ae23b96725ce214ce2fc90812
Manish <manish2003sharma@gmail.com>
2025-06-29 00:29:13 +0530

    Personal Finance Tracker

---
+++ src/firebase.js
@@ -0,0 +1,27 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore, doc, setDoc } from "firebase/firestore";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCoA4YYgQ0UuLMViuKtRikwGeKZV4HUfSY",
+    authDomain: "financy-2b078.firebaseapp.com",
+    projectId: "financy-2b078",
+    storageBucket: "financy-2b078.firebasestorage.app",
+    messagingSenderId: "700028242397",
+    appId: "1:700028242397:web:2e4e23a1886757d2fb4e34",
+    measurementId: "G-W16D05CN2W"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const db = getFirestore(app);
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+export { db, auth, provider, doc, setDoc };
