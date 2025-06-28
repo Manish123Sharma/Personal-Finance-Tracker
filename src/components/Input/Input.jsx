d86f35ee874caf2732f8ac9f8b638adddb95b6df
Manish <manish2003sharma@gmail.com>
2025-06-29 00:29:47 +0530

    Custom Input

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import './Input.css'
 
-const Input = () => {
+const Input = ({ label, state, setState, placeholder, type }) => {
     return (
+        // <div className='input_wrapper'>
+        //     <input type="text" className='input_field' placeholder='Enter text...' />
+        // </div>
         <div className='input_wrapper'>
-            <input type="text" className='input_field' placeholder='Enter text...' />
+            <p className='input_label'>
+                {label}
+            </p>
+            <input
+                placeholder={placeholder} value={state} onChange={(e) => setState(e.target.value)} className='input_field' type={type} />
         </div>
     );
 }
