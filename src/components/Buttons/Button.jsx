e75274b126ca36162fb63a8ed7af5f1c2e36e46e
Manish <manish2003sharma@gmail.com>
2025-06-29 17:27:13 +0530

    Custom Buttons

--- src/components/Buttons/Button.jsx
+++ src/components/Buttons/Button.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
+import PropTypes from 'prop-types';
 import './Button.css'
 
-const Button = ({text, onCLick, blue}) => {
+const Button = ({text, onClick, blue, disabled}) => {
     return (
-        <button className={blue ? 'btn btn_blue': 'btn'} onClick={onCLick} type="button">
+        <button className={blue ? 'btn btn_blue': 'btn'} onClick={onClick} type="button" disabled={disabled}>
             {text}
         </button>
     );
 }
+
+Button.propTypes = {
+    text: PropTypes.string.isRequired,
+    onClick: PropTypes.func.isRequired,
+    blue: PropTypes.bool,
+    disabled: PropTypes.bool
+};
 
 export default Button;
