2382eda1e7ab3d64a7ccaa197c376dd72f3eb0c4
Manish <manish2003sharma@gmail.com>
2025-07-02 15:10:16 +0530

    Custom Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,39 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './Header.css'
 import logo from '../../assets/logo.svg'
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth } from '../../firebase';
+import { useNavigate } from "react-router-dom";
+import { signOut } from "firebase/auth";
+import { toast } from 'react-toastify';
 
 const Header = () => {
+    const navigate = useNavigate();
     const handlelogout = () => {
-        console.log(`Logging out...`);
-        alert('Logout successful');
+
+        try {
+            signOut(auth).then(() => {
+                toast.success("Logged out successfully");
+                navigate('/');
+                // Sign-out successful.
+            }).catch((error) => {
+                console.error("Logout error", error);
+                toast.error(error.message);
+                // An error happened.
+            });
+        } catch (error) {
+            console.log("Error in logout", error);
+            toast.error(error.message);
+
+        }
+
     }
+    const [user, loading] = useAuthState(auth);
+    useEffect(() => {
+        if (user) {
+            navigate('/dashboard');
+        }
+    }, [user, loading, navigate]);
     return (
         <div className='navbar'>
             <div className='logo'>
@@ -16,13 +43,14 @@ const Header = () => {
                     <span>Personal Finance Tracker</span>
                 </div>
             </div>
-            <button
+            {user && <button
                 type="button"
                 className="logout-button"
                 onClick={() => handlelogout()}
             >
                 ⏻ Logout
-            </button>
+            </button>}
+
             {/* <p>This is header</p> */}
         </div>
     )
