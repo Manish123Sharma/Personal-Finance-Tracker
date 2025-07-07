9713e95c3cee9ce042d4bbfe4f3b7c2f956bcadf
Manish <manish2003sharma@gmail.com>
2025-07-07 14:27:45 +0530

    Custom Cards

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 import './Cards.css';
 import { Card, Row } from 'antd';
 import Button from '../Buttons/Button';
+import PropTypes from 'prop-types';
 
-const Cards = ({ showExpenseModal, showIncomeModal }) => {
+const Cards = ({ showExpenseModal, showIncomeModal, currentBalance, income, expenses }) => {
     return (
         <div>
             <Row className='my_row'>
@@ -21,24 +22,31 @@ const Cards = ({ showExpenseModal, showIncomeModal }) => {
                 </Card> */}
                 <Card  className='my_card'>
                     <h2>Current Balance</h2>
-                    <p>₹0</p>
+                    <p>₹ {currentBalance}</p>
                     <Button blue={true} text={'Reset Balance'} />
                 </Card>
 
                 <Card  className='my_card'>
                     <h2>Total Income</h2>
-                    <p>₹0</p>
+                    <p>₹ {income}</p>
                     <Button blue={true} text={'Add Income'} onClick={showIncomeModal} />
                 </Card>
 
                 <Card  className='my_card'>
                     <h2>Total Expenses</h2>
-                    <p>₹0</p>
+                    <p>₹ {expenses}</p>
                     <Button blue={true} text={'Add Expense'} onClick={showExpenseModal} />
                 </Card>
             </Row>
         </div>
     );
+};
+Cards.propTypes = {
+    showExpenseModal: PropTypes.func.isRequired,
+    showIncomeModal: PropTypes.func.isRequired,
+    currentBalance: PropTypes.number.isRequired,
+    income: PropTypes.number.isRequired,
+    expenses: PropTypes.number.isRequired,
 };
 
 export default Cards;
